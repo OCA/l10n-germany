2bb93765739f18aa5ee69f0c78771671270f660e
Nikolina Todorova <nikolina.todorova@initos.com>
2014-12-15 08:51:06 +0100

    Change the import

--- l10n_de_country_states/__init__.py
+++ l10n_de_country_states/__init__.py
@@ -20,4 +20,4 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from res_country import *
+from . import res_country
