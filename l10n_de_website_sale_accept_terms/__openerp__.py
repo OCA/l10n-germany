7ac7d855aca405be61cfddd8d0006f40c13ce31c
unknown <mn@copado.de>
2015-05-15 09:17:23 +0200

    update __openerp__.py with several informations, add readme.rst

--- l10n_de_website_sale_accept_terms/__openerp__.py
+++ l10n_de_website_sale_accept_terms/__openerp__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2013 OpenERP s.a. (<http://openerp.com>).
-#    Copyright (C) 2014 copado MEDIA UG (<http://www.copado.de>).
-#    Author Mathias Neef <mn[at]copado.de>
+#    Author: Mathias Neef
+#    Copyright 2015 copadoMEDIA UG
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -25,35 +23,9 @@
     'category': 'Website',
     'summary': 'German additions for eCommerce (B2C)',
     'version': '1.0',
-    'description': """
-German additions for eCommerce (B2C)
-==========================
-
-This addon installes necessary additions in accordance with statutory
-requirements in Germany.
-
-Overview
---------
- - adds terms page
- - adds revocation page
- - adds delivery page
- - adds privacy page
- - adds imprint page
- - adds a note on payment site, that terms and revocation are accepted with
-   click on "Pay now"
- - adds terms and revocation popup on payment site; both are taken from terms
-   and revocation page on the frontend
-
-
-Contact for questions
----------------------
-copado MEDIA UG - Unterdorfstr. 29 - 77948 Friesenheim - Germany
-Phone: +49 7821 32725 20 - info@copado.de - http:www.copado.de
---> Thanks to Markus Schneider from initos who helped me a lot
-with this first addon <--
-        """,
-    'author': 'copado MEDIA UG, Mathias Neef',
+    'author': 'copado MEDIA UG, Odoo Community Association (OCA)',
     'website': 'http://www.copado.de',
+    'license': 'AGPL-3',
     'depends': ['website_sale'],
     'data': ['views/views.xml'],
     'demo': [],
---
+++ l10n_de_website_sale_accept_terms/readme.rst
@@ -0,0 +1,25 @@
+German additions for eCommerce (B2C)
+==========================
+
+This addon installes necessary additions in accordance with statutory
+requirements in Germany.
+
+Overview
+--------
+ - adds terms page
+ - adds revocation page
+ - adds delivery page
+ - adds privacy page
+ - adds imprint page
+ - adds a note on payment site, that terms and revocation are accepted with
+   click on "Pay now"
+ - adds terms and revocation popup on payment site; both are taken from terms
+   and revocation page on the frontend
+
+
+Contact for questions
+---------------------
+copado MEDIA UG - Unterdorfstr. 29 - 77948 Friesenheim - Germany
+Phone: +49 7821 32725 20 - info@copado.de - http:www.copado.de
+--> Thanks to Markus Schneider from initos who helped me a lot
+with this first addon <--
