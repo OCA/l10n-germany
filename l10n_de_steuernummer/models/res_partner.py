aa2d7c2bd946b22ad371d662f4fc64a855d40332
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-22 10:41:26 +0200

    [MIG] l10n_de_steuernummer: Migration to 10.0

--- l10n_de_steuernummer/models/res_partner.py
+++ l10n_de_steuernummer/models/res_partner.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
-# See README.rst file on addon root folder for more details
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import re
-from openerp import models
+from odoo import api, models
 
 try:
     import vatnumber
@@ -14,15 +15,14 @@ except ImportError:
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    @api.model
     def check_vat_de(self, vat):
         res = False
-        official = getattr(vatnumber, 'check_vat_de', None)
-        if official:
-            res = official(vat)
+        if getattr(vatnumber, 'check_vat_de', None):
+            res = vatnumber.check_vat_de(vat)
         if not res:
-            # This vat is not a Steue-IdNr., so if it is 10 or 11 digit length
-            # then we can assume that this is a old Steuernummer
+            # This vat is not a Steuer-IdNr., so if it is 10 or 11 digit
+            # length then we can assume that this is a old Steuernummer
             vat = re.sub(r'[^0-9]', '', vat)
-            if len(vat) >= 10 and len(vat) <= 11:
-                res = True
+            return 10 <= len(vat) <= 11
         return res
