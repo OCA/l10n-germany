e856a487bd2d3ccb7cb1642e95dc8cc623664b2d
ralwafaie <rami.alwafaie@initos.de>
2018-01-25 11:55:00 +0100

    remove unwanted import

--- l10n_de_steuernummer/tests/test_l10n_de_steuernummer.py
+++ l10n_de_steuernummer/tests/test_l10n_de_steuernummer.py
@@ -1,7 +1,6 @@
 # Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.exceptions import ValidationError
 from odoo.tests import common
 
 
