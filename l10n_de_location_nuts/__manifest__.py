75c629d2257dcbed70e617e99fd79032040220a1
ralwafaie <rami.alwafaie@initos.de>
2018-02-01 09:43:21 +0100

    [MIG] l10n_de_location_nuts: Migration to 11.0

--- l10n_de_location_nuts/__manifest__.py
+++ l10n_de_location_nuts/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
-# Copyright 2015 Jairo Llopis <jairo.llopis@tecnativa.com>
-# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
+# Copyright 2015 Tecnativa - Antonio Espinosa
+# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2015 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'NUTS Regions for German',
     'summary': 'NUTS specific options for German',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Localisation/Europe',
     'website': 'http://www.tecnativa.com',
     'author': 'Tecnativa, '
