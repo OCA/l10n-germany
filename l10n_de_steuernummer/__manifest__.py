a9f56482e7abf2b8fd3466e01fe617a6c9ba20c6
ralwafaie <rami.alwafaie@initos.de>
2018-02-02 09:39:57 +0100

    [MIG] l10n_de_steuernummer: Migration to 11.0

--- l10n_de_steuernummer/__manifest__.py
+++ l10n_de_steuernummer/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,7 +5,7 @@
 {
     'name': "German SteuerNummer validation",
     'category': 'Localisation/Europe',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'depends': [
         'base_vat',
     ],
