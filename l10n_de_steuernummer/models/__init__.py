f3bf7556a177496a2cfcce06a21a1e80b8038a83
ralwafaie <rami.alwafaie@initos.de>
2018-02-02 09:39:57 +0100

    [MIG] l10n_de_steuernummer: Migration to 11.0

--- l10n_de_steuernummer/models/__init__.py
+++ l10n_de_steuernummer/models/__init__.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
-# See README.rst file on addon root folder for more details
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import res_partner
