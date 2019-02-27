df6b00644d9235045ddb179e7f54e3c0eb6317c5
Thorsten Vocks <thorsten.vocks@openbig.org>
2019-02-27 16:49:19 +0100

    mainly fixes to correct pylint and flake errors

--- l10n_de_tax_statement/wizard/l10n_de_tax_statement_config_wizard.py
+++ l10n_de_tax_statement/wizard/l10n_de_tax_statement_config_wizard.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 BIG-Consulting GmbH (<http://www.openbig.org>)
 # Copyright 2019 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
