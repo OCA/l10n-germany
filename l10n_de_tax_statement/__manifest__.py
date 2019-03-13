b32e3f362062b4852cabd3bc25b6c33e961112f4
Thorsten Vocks <thorsten.vocks@openbig.org>
2019-03-13 12:06:28 +0100

    further fixes and readme documents

--- l10n_de_tax_statement/__manifest__.py
+++ l10n_de_tax_statement/__manifest__.py
@@ -4,15 +4,14 @@
 
 {
     'name': 'German VAT Statement',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.0.0',
     'category': 'Localization',
     'license': 'AGPL-3',
     'author': 'OpenBIG.org, Onestein, Odoo Community Association (OCA)',
-    'website': 'https://github.com/openbig/l10n-germany',
+    'website': 'https://github.com/OCA/l10n-germany',
     'depends': [
         'account_invoicing',
         'account_tax_balance',
-        'l10n_de',
     ],
     'data': [
         'security/ir.model.access.csv',
