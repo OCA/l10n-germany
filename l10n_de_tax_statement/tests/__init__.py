dc64f84b1060717687b44282f95301a07aa9247a
Andrea <a.stirpe@onestein.nl>
2019-03-25 15:37:19 +0100

    [12.0][MIG] l10n_de_tax_statement

--- l10n_de_tax_statement/tests/__init__.py
+++ l10n_de_tax_statement/tests/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2019 BIG-Consulting GmbH(<http://www.openbig.org>)
-# Copyright 2019 Onestein(<http://www.onestein.eu>)
-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import test_l10n_de_tax_statement
