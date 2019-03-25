7955168629fed7f8de87811cd3141028b17e5055
Andrea <a.stirpe@onestein.nl>
2019-03-25 15:37:19 +0100

    [12.0][MIG] l10n_de_tax_statement

--- l10n_de_tax_statement/__init__.py
+++ l10n_de_tax_statement/__init__.py
@@ -1,6 +1,4 @@
-# Copyright 2019 Onestein (<https://www.onestein.eu>)
-# Copyright 2019 BIG-Consulting GmbH(<http://www.openbig.org>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import models
 from . import wizard
