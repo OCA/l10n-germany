26a78422739d29f1e7696475f7c9d7cfff6719dd
Andrea <a.stirpe@onestein.nl>
2019-08-14 10:05:14 +0200

    Code review

--- l10n_de_tax_statement/models/l10n_de_tax_statement_2019.py
+++ l10n_de_tax_statement/models/l10n_de_tax_statement_2019.py
@@ -5,220 +5,220 @@
 from odoo import _
 
 
-tax_statement_dict_2019 = {
-    '17': {
-        'code': '17',
-        'name': _('Anmeldung der Umsatzsteuer Vorauszahlung'),
-    },
-    '18': {
-        'code': '18',
-        'name': _('Lief. u. sonst. Leistg. einschl. unentg. Wertabg.'),
-    },
-    '19': {
-        'code': '19',
-        'name': _('Steuerfr. Umsätze mit Vorsteuerabz. innerg. '
-                  'Lieferungen (§4 Nr. 1b) ...'),
-    },
-    '20': {
-        'code': '20', 'base': 0.0,
-        'name': _('... an Abnehmer mit USt-ID (41)'),
-    },
-    '21': {
-        'code': '21', 'base': 0.0,
-        'name': _('... neuer Fahrzeuge an Abnehmer ohne UST-ID (44)'),
-    },
-    '22': {
-        'code': '22', 'base': 0.0,
-        'name': _('... neuer Fahrzeuge außerh. eines Unternehmens '
-                  '§ 2a UStG (49)'),
-    },
-    '23': {
-        'code': '23', 'base': 0.0,
-        'name': _('Weitere steuerfr. Umsätze mit Vorsteuerabzug, '
-                  'z.B. Ausfuhrlief., Umsätze n. § 4 Nr. 2-7 UStG (43)'),
-    },
-    '24': {
-        'code': '24', 'base': 0.0,
-        'name': _('Steuerfreie Umsätze ohne Vorsteuerabzug '
-                  'Umsätze n. § 4 Nr. 8 bis 28 UStG (48)'),
-    },
-    '25': {
-        'code': '25',
-        'name': _('Steuerpflichtige Umsätze '
-                  '(Lief. u. sonst. Leistg. einschl. unentg. Wertabg.)'),
-    },
-    '26': {
-        'code': '26', 'base': 0.0, 'tax': 0.0,
-        'name': _('... zum Steuersatz von 19 % (81)'),
-    },
-    '27': {
-        'code': '27', 'base': 0.0, 'tax': 0.0,
-        'name': _('... zum Steuersatz von 7% (86)'),
-    },
-    '28': {
-        'code': '28', 'base': 0.0, 'tax': 0.0,
-        'name': _('... zu anderen Steuersätzen (35 / 36)'),
-    },
-    '29': {
-        'code': '29', 'base': 0.0,
-        'name': _('Lieferungen land- u. forstw. Betriebe '
-                  'nach § 24 UStG an Abnehmer mit Ust-ID (77)'),
-    },
-    '30': {
-        'code': '30', 'base': 0.0, 'tax': 0.0,
-        'name': _('Umsätze nach § 24 UStG, '
-                  'z.B. Sägewerke, Getränke u. alk. Flüssigk. (76 / 80)'),
-    },
-    '31': {
-        'code': '31',
-        'name': _('Innergemeinschaftliche Erwerbe '
-                  'Steuerfreie innergemeinschaftliche Erwerbe'),
-    },
-    '32': {
-        'code': '32', 'base': 0.0,
-        'name': _('Erwerbe nach §§ 4b u. 25c UStG (91)'),
-    },
-    '33': {
-        'code': '33', 'base': 0.0, 'tax': 0.0,
-        'name': _('Steuerpflichtige innergemeinschaftliche Erwerbe '
-                  '... zum Steuersatz v. 19 % (89)'),
-    },
-    '34': {
-        'code': '34', 'base': 0.0, 'tax': 0.0,
-        'name': _('... zum Steuersatz v. 7% (93)'),
-    },
-    '35': {
-        'code': '35', 'base': 0.0, 'tax': 0.0,
-        'name': _('... zu anderen Steuersätzen (95 / 98)'),
-    },
-    '36': {
-        'code': '36', 'base': 0.0, 'tax': 0.0,
-        'name': _('... neuer Fahrzeuge gem. § 1b Abs. 2 u. 3 UStG '
-                  'von Lieferern o. Ust-ID z. allg. Steuersatz (94 / 96)'),
-    },
-    '37': {
-        'code': '37',
-        'name': _('Ergänzende Angaben zu Umsätzen'),
-    },
-    '38': {
-        'code': '38', 'base': 0.0,
-        'name': _('Lieferungen des ersten Abnehmers bei innergem. '
-                  'Dreiecksgeschäften gem. § 25b Abs. 2 UStG (42)'),
-    },
-    '39': {
-        'code': '39', 'base': 0.0,
-        'name': _('Übrige steuerpfl. Umsätze f.d.d. Lstg.empf. d. Steuer '
-                  'n. § 13b Abs. 5 UStG schuldet (60)'),
-    },
-    '40': {
-        'code': '40', 'base': 0.0,
-        'name': _('Nicht steuerb. sonst. Leist. gem. '
-                  '§ 18b S. 1 Nr. 2 (21)'),
-    },
-    '41': {
-        'code': '41', 'base': 0.0,
-        'name': _('Übrige n. steuerb. Umsätze, Leistungsort '
-                  'ist nicht im Inland (45)'),
-    },
-    '47': {
-        'code': '47',
-        'name': _('Leistungsempfänger als Steuerschuldner '
-                  '(§ 13b UStG)'),
-    },
-    '48': {
-        'code': '48', 'base': 0.0, 'tax': 0.0,
-        'name': _('Steuerpfl. sonst. Leist. e. i. übr. Gemeinschaftsgeb. '
-                  'ans. Untern. gem. § 13b Abs. 1 UStG (46 / 47)'),
-    },
-    '49': {
-        'code': '49', 'base': 0.0, 'tax': 0.0,
-        'name': _('Lieferungen sicherungsübereign. Gegenst. u. Umsätze '
-                  'd. u. d.  GrEStG fallen g. § 13b Abs. 2 '
-                  'Nr. 3 (73 / 74)'),
-    },
-    '50': {
-        'code': '50', 'base': 0.0, 'tax': 0.0,
-        'name': _('Andere Leistungen gem. § 13b Abs. 2 Nr. 1, 2,'
-                  '4 b. 11 UStG (84 / 85)'),
-    },
-    '51': {
-        'code': '51', 'tax': 0.0,
-        'name': _('Umsatzsteuer'),
-    },
-    '52': {
-        'code': '52',
-        'name': _('Abziehbare Vorsteuerbeträge'),
-    },
-    '53': {
-        'code': '53', 'tax': 0.0,
-        'name': _('Vorsteuerbeträge aus Rechn. v.a. Unternehmen g. § 15 '
-                  'Abs. S. 1 Nr. 1 UStG a. Leistungen i.S.d. § 13a '
-                  'Abs. 1 Nr. 6 UStG u. § 15 Abs. 1 S. 1 Nr. 5 UStG '
-                  'u. a. innerg. Dreiecksgesch. g. § 25b A. 5 UStG (66)'),
-    },
-    '54': {
-        'code': '54', 'tax': 0.0,
-        'name': _('Vorsteuerbeträge a. d. innerg. Erwerb v. Gegenständen '
-                  'gem. § 15 Abs. 1 Satz 1 Nr. 3 UStG (61)'),
-    },
-    '55': {
-        'code': '55', 'tax': 0.0,
-        'name': _('Entst. Einfuhrumsatzst. g. § 15 Abs. 1 S. 1 Nr. 2 '
-                  'UStG (62)'),
-    },
-    '56': {
-        'code': '56', 'tax': 0.0,
-        'name': _('Vorsteuerbeträge aus Leistungen i. S. des § 13b UStG'
-                  'i.V.m § 15 Abs. 1 Satz 1 Nr. 4 UStG (67)'),
-    },
-    '57': {
-        'code': '57', 'tax': 0.0,
-        'name': _('Vorsteuerbeträge d. n. allg. Durchschnittssätzen '
-                  'berechnet sind gem. §§ 23 und 23a UStG (63)'),
-    },
-    '58': {
-        'code': '58', 'tax': 0.0,
-        'name': _('Berichtigung des Vorsteuerabzugs g. § 15 a UStG (64)'),
-    },
-    '59': {
-        'code': '59', 'tax': 0.0,
-        'name': _('Vorsteuerabzug f. innergem. Lief. neuer Fahrzeuge '
-                  'außerh. e. Untern. g. §2a UStG sow. v. Kleinunt. i.S. '
-                  'd. § 19 Abs. 1 i.V.m. § 15a Abs. 4a UStG (59)'),
-    },
-    '60': {
-        'code': '60', 'tax': 0.0,
-        'name': _('Verbleibender Betrag'),
-    },
-    '61': {
-        'code': '61',
-        'name': _('Andere Steuerbeträge'),
-    },
-    '62': {
-        'code': '62', 'tax': 0.0,
-        'name': _('Steuer inf. Wechsels d. Besteuerungsf. sow. Nachst. '
-                  'a. verst. Anzahlungen u.a. wg. Steuersatzänd. (65)'),
-    },
-    '63': {
-        'code': '63', 'tax': 0.0,
-        'name': _('In Rechnungen unrichtig oder unberechtigt ausgewiesene '
-                  'Steuerbeträge gem. § 14c UstG) sowie Steuerbetr. d. n. '
-                  '§ 6a Abs. 4 S. 2, § 17 Abs. 1 S. 6, § 25 b Abs. 2 UStG '
-                  'o. v. e. Auslagerer o. Lagerh. n. § 13a Abs. 1 Nr. 6 '
-                  'UStG geschuldet werden (69)'),
-    },
-    '64': {
-        'code': '64', 'tax': 0.0,
-        'name': _('Umsatzsteuer-Vorauszahlung'),
-    },
-    '65': {
-        'code': '65', 'tax': 0.0,
-        'name': _('Abzug der festges. Sondervorauszahl. f. '
-                  'Dauerfristverlängerung, nur auszuf. i. d. letzten '
-                  'Voranmeldung d. Besteuerungszeitr., i.d.R. Dez. (39)'),
-    },
-
-}
+def _tax_statement_dict_2019():
+    return {
+        '17': {
+            'code': '17',
+            'name': _('Anmeldung der Umsatzsteuer Vorauszahlung'),
+        },
+        '18': {
+            'code': '18',
+            'name': _('Lief. u. sonst. Leistg. einschl. unentg. Wertabg.'),
+        },
+        '19': {
+            'code': '19',
+            'name': _('Steuerfr. Umsätze mit Vorsteuerabz. innerg. '
+                      'Lieferungen (§4 Nr. 1b) ...'),
+        },
+        '20': {
+            'code': '20', 'base': 0.0,
+            'name': _('... an Abnehmer mit USt-ID (41)'),
+        },
+        '21': {
+            'code': '21', 'base': 0.0,
+            'name': _('... neuer Fahrzeuge an Abnehmer ohne UST-ID (44)'),
+        },
+        '22': {
+            'code': '22', 'base': 0.0,
+            'name': _('... neuer Fahrzeuge außerh. eines Unternehmens '
+                      '§ 2a UStG (49)'),
+        },
+        '23': {
+            'code': '23', 'base': 0.0,
+            'name': _('Weitere steuerfr. Umsätze mit Vorsteuerabzug, '
+                      'z.B. Ausfuhrlief., Umsätze n. § 4 Nr. 2-7 UStG (43)'),
+        },
+        '24': {
+            'code': '24', 'base': 0.0,
+            'name': _('Steuerfreie Umsätze ohne Vorsteuerabzug '
+                      'Umsätze n. § 4 Nr. 8 bis 28 UStG (48)'),
+        },
+        '25': {
+            'code': '25',
+            'name': _('Steuerpflichtige Umsätze '
+                      '(Lief. u. sonst. Leistg. einschl. unentg. Wertabg.)'),
+        },
+        '26': {
+            'code': '26', 'base': 0.0, 'tax': 0.0,
+            'name': _('... zum Steuersatz von 19 % (81)'),
+        },
+        '27': {
+            'code': '27', 'base': 0.0, 'tax': 0.0,
+            'name': _('... zum Steuersatz von 7% (86)'),
+        },
+        '28': {
+            'code': '28', 'base': 0.0, 'tax': 0.0,
+            'name': _('... zu anderen Steuersätzen (35 / 36)'),
+        },
+        '29': {
+            'code': '29', 'base': 0.0,
+            'name': _('Lieferungen land- u. forstw. Betriebe '
+                      'nach § 24 UStG an Abnehmer mit Ust-ID (77)'),
+        },
+        '30': {
+            'code': '30', 'base': 0.0, 'tax': 0.0,
+            'name': _('Umsätze nach § 24 UStG, '
+                      'z.B. Sägewerke, Getränke u. alk. Flüssigk. (76 / 80)'),
+        },
+        '31': {
+            'code': '31',
+            'name': _('Innergemeinschaftliche Erwerbe '
+                      'Steuerfreie innergemeinschaftliche Erwerbe'),
+        },
+        '32': {
+            'code': '32', 'base': 0.0,
+            'name': _('Erwerbe nach §§ 4b u. 25c UStG (91)'),
+        },
+        '33': {
+            'code': '33', 'base': 0.0, 'tax': 0.0,
+            'name': _('Steuerpflichtige innergemeinschaftliche Erwerbe '
+                      '... zum Steuersatz v. 19 % (89)'),
+        },
+        '34': {
+            'code': '34', 'base': 0.0, 'tax': 0.0,
+            'name': _('... zum Steuersatz v. 7% (93)'),
+        },
+        '35': {
+            'code': '35', 'base': 0.0, 'tax': 0.0,
+            'name': _('... zu anderen Steuersätzen (95 / 98)'),
+        },
+        '36': {
+            'code': '36', 'base': 0.0, 'tax': 0.0,
+            'name': _('... neuer Fahrzeuge gem. § 1b Abs. 2 u. 3 UStG '
+                      'von Lieferern o. Ust-ID z. allg. Steuersatz (94 / 96)'),
+        },
+        '37': {
+            'code': '37',
+            'name': _('Ergänzende Angaben zu Umsätzen'),
+        },
+        '38': {
+            'code': '38', 'base': 0.0,
+            'name': _('Lieferungen des ersten Abnehmers bei innergem. '
+                      'Dreiecksgeschäften gem. § 25b Abs. 2 UStG (42)'),
+        },
+        '39': {
+            'code': '39', 'base': 0.0,
+            'name': _('Übrige steuerpfl. Umsätze f.d.d. Lstg.empf. d. Steuer '
+                      'n. § 13b Abs. 5 UStG schuldet (60)'),
+        },
+        '40': {
+            'code': '40', 'base': 0.0,
+            'name': _('Nicht steuerb. sonst. Leist. gem. '
+                      '§ 18b S. 1 Nr. 2 (21)'),
+        },
+        '41': {
+            'code': '41', 'base': 0.0,
+            'name': _('Übrige n. steuerb. Umsätze, Leistungsort '
+                      'ist nicht im Inland (45)'),
+        },
+        '47': {
+            'code': '47',
+            'name': _('Leistungsempfänger als Steuerschuldner '
+                      '(§ 13b UStG)'),
+        },
+        '48': {
+            'code': '48', 'base': 0.0, 'tax': 0.0,
+            'name': _('Steuerpfl. sonst. Leist. e. i. übr. Gemeinschaftsgeb. '
+                      'ans. Untern. gem. § 13b Abs. 1 UStG (46 / 47)'),
+        },
+        '49': {
+            'code': '49', 'base': 0.0, 'tax': 0.0,
+            'name': _('Lieferungen sicherungsübereign. Gegenst. u. Umsätze '
+                      'd. u. d.  GrEStG fallen g. § 13b Abs. 2 '
+                      'Nr. 3 (73 / 74)'),
+        },
+        '50': {
+            'code': '50', 'base': 0.0, 'tax': 0.0,
+            'name': _('Andere Leistungen gem. § 13b Abs. 2 Nr. 1, 2,'
+                      '4 b. 11 UStG (84 / 85)'),
+        },
+        '51': {
+            'code': '51', 'tax': 0.0,
+            'name': _('Umsatzsteuer'),
+        },
+        '52': {
+            'code': '52',
+            'name': _('Abziehbare Vorsteuerbeträge'),
+        },
+        '53': {
+            'code': '53', 'tax': 0.0,
+            'name': _('Vorsteuerbeträge aus Rechn. v.a. Unternehmen g. § 15 '
+                      'Abs. S. 1 Nr. 1 UStG a. Leistungen i.S.d. § 13a '
+                      'Abs. 1 Nr. 6 UStG u. § 15 Abs. 1 S. 1 Nr. 5 UStG '
+                      'u. a. innerg. Dreiecksgesch. g. § 25b A. 5 UStG (66)'),
+        },
+        '54': {
+            'code': '54', 'tax': 0.0,
+            'name': _('Vorsteuerbeträge a. d. innerg. Erwerb v. Gegenständen '
+                      'gem. § 15 Abs. 1 Satz 1 Nr. 3 UStG (61)'),
+        },
+        '55': {
+            'code': '55', 'tax': 0.0,
+            'name': _('Entst. Einfuhrumsatzst. g. § 15 Abs. 1 S. 1 Nr. 2 '
+                      'UStG (62)'),
+        },
+        '56': {
+            'code': '56', 'tax': 0.0,
+            'name': _('Vorsteuerbeträge aus Leistungen i. S. des § 13b UStG'
+                      'i.V.m § 15 Abs. 1 Satz 1 Nr. 4 UStG (67)'),
+        },
+        '57': {
+            'code': '57', 'tax': 0.0,
+            'name': _('Vorsteuerbeträge d. n. allg. Durchschnittssätzen '
+                      'berechnet sind gem. §§ 23 und 23a UStG (63)'),
+        },
+        '58': {
+            'code': '58', 'tax': 0.0,
+            'name': _('Berichtigung des Vorsteuerabzugs g. § 15 a UStG (64)'),
+        },
+        '59': {
+            'code': '59', 'tax': 0.0,
+            'name': _('Vorsteuerabzug f. innergem. Lief. neuer Fahrzeuge '
+                      'außerh. e. Untern. g. §2a UStG sow. v. Kleinunt. i.S. '
+                      'd. § 19 Abs. 1 i.V.m. § 15a Abs. 4a UStG (59)'),
+        },
+        '60': {
+            'code': '60', 'tax': 0.0,
+            'name': _('Verbleibender Betrag'),
+        },
+        '61': {
+            'code': '61',
+            'name': _('Andere Steuerbeträge'),
+        },
+        '62': {
+            'code': '62', 'tax': 0.0,
+            'name': _('Steuer inf. Wechsels d. Besteuerungsf. sow. Nachst. '
+                      'a. verst. Anzahlungen u.a. wg. Steuersatzänd. (65)'),
+        },
+        '63': {
+            'code': '63', 'tax': 0.0,
+            'name': _('In Rechnungen unrichtig oder unberechtigt ausgewiesene '
+                      'Steuerbeträge gem. § 14c UstG) sowie Steuerbetr. d. n. '
+                      '§ 6a Abs. 4 S. 2, § 17 Abs. 1 S. 6, § 25 b Abs. 2 UStG '
+                      'o. v. e. Auslagerer o. Lagerh. n. § 13a Abs. 1 Nr. 6 '
+                      'UStG geschuldet werden (69)'),
+        },
+        '64': {
+            'code': '64', 'tax': 0.0,
+            'name': _('Umsatzsteuer-Vorauszahlung'),
+        },
+        '65': {
+            'code': '65', 'tax': 0.0,
+            'name': _('Abzug der festges. Sondervorauszahl. f. '
+                      'Dauerfristverlängerung, nur auszuf. i. d. letzten '
+                      'Voranmeldung d. Besteuerungszeitr., i.d.R. Dez. (39)'),
+        },
+    }
 
 
 def _finalize_lines_2019(lines):
