316709041b86dab62c168d328486f93001afe166
Yu Weng <yweng@elegosoft.com>
2019-08-27 17:35:48 +0200

    [ADD] l10n_de_holidays addon

---
+++ l10n_de_holidays/tests/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2018 elego Software Solutions GmbH - Yu Weng
+# Copyright 2018 initOS GmbH - Nikolina Todorova
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_hr_holidays_public_generator
