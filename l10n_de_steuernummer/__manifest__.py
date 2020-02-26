d344e41035635b24231da3265e96ecba1a8a97a5
Achraf Mhadhbi <machraf@bloopark.de>
2020-02-26 17:13:57 +0100

    [MIG] l10n_de_steuernummer: Migration to 13.0

--- l10n_de_steuernummer/__manifest__.py
+++ l10n_de_steuernummer/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "German SteuerNummer validation",
     'category': 'Localisation/Europe',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'depends': [
         'base_vat',
     ],
--- l10n_de_steuernummer/readme/CONTRIBUTORS.rst
+++ l10n_de_steuernummer/readme/CONTRIBUTORS.rst
@@ -3,3 +3,4 @@
 * Pedro M. Baeza <pedro.baeza@tecnativa.com>
 * Rami Alwafaie <rami.alwafaie@initos.com>
 * Thore Baden
+* Achraf Mhadhbi <machraf@bloopark.de>
