1ad4b26bc94200a39b0fa5857f559c06eb82ec99
Andrea <a.stirpe@onestein.nl>
2020-04-17 15:18:26 +0200

    [13.0][MIG] l10n_de_tax_statement

--- l10n_de_tax_statement/__manifest__.py
+++ l10n_de_tax_statement/__manifest__.py
@@ -1,15 +1,15 @@
-# Copyright 2019 Onestein (<https://www.onestein.eu>)
+# Copyright 2019-2020 Onestein (<https://www.onestein.eu>)
 # Copyright 2019 BIG-Consulting GmbH(<http://www.openbig.org>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "German VAT Statement",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "category": "Localization",
     "license": "AGPL-3",
-    "author": "OpenBIG.org, Onestein, Odoo Community Association (OCA)",
+    "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-germany",
-    "depends": ["account_tax_balance",],
+    "depends": ["account", "date_range"],
     "data": [
         "security/ir.model.access.csv",
         "security/tax_statement_security_rule.xml",
@@ -18,7 +18,6 @@
         "views/l10n_de_tax_statement_view.xml",
         "views/report_tax_statement.xml",
         "report/report_tax_statement.xml",
-        "wizard/l10n_de_tax_statement_config_wizard.xml",
     ],
     "installable": True,
 }
