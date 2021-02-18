76f0c8359323703b5f28e76acde96314dab39917
fshah <foram.shah@initos.com>
2021-02-18 15:34:37 +0530

    [IMP] l10n_de_country_states: black, isort, prettier

--- l10n_de_country_states/__manifest__.py
+++ l10n_de_country_states/__manifest__.py
@@ -5,19 +5,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'German Country States',
-    'category': 'Localisation/Europe',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'initOS GmbH'
-              'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-germany',
-    'depends': [
-        'base_country_state_translatable',
+    "name": "German Country States",
+    "category": "Localisation/Europe",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "initOS GmbH" "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-germany",
+    "depends": [
+        "base_country_state_translatable",
     ],
-    'data': [
-        'data/res_country_states.xml'
-    ],
-    'installable': True,
+    "data": ["data/res_country_states.xml"],
+    "installable": True,
 }
