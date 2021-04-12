5293daf2b60205fb192ad853221345abdc3defca
fshaha <foram.shah@initos.com>
2021-04-12 11:11:25 +0530

    [IMP] l10n_de_location_nuts: Isort, black, perttier.

--- l10n_de_location_nuts/__manifest__.py
+++ l10n_de_location_nuts/__manifest__.py
@@ -4,19 +4,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'NUTS Regions for German',
-    'summary': 'NUTS specific options for German',
-    'version': '12.0.1.0.0',
-    'category': 'Localisation/Europe',
-    'website': 'https://github.com/OCA/l10n-germany',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'application': False,
-    'depends': [
-        'base_location_nuts',
-        'l10n_de_country_states',
+    "name": "NUTS Regions for German",
+    "summary": "NUTS specific options for German",
+    "version": "14.0.1.0.0",
+    "category": "Localisation/Europe",
+    "website": "https://github.com/OCA/l10n-germany",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "base_location_nuts",
+        "l10n_de_country_states",
     ],
-    'post_init_hook': 'post_init_hook',
-    'installable': True,
+    "post_init_hook": "post_init_hook",
+    "installable": True,
 }
--- l10n_de_location_nuts/hooks.py
+++ l10n_de_location_nuts/hooks.py
@@ -3,8 +3,9 @@
 # Copyright 2015 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, SUPERUSER_ID
 import logging
+
+from odoo import SUPERUSER_ID, api
 
 _logger = logging.getLogger(__name__)
 
@@ -13,8 +14,10 @@ def post_init_hook(cr, registry):
     """Define German specific configuration in res.country."""
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        germany = env.ref('base.de')
-        _logger.info('Setting Germany NUTS configuration')
-        germany.write({
-            'state_level': 2,
-        })
+        germany = env.ref("base.de")
+        _logger.info("Setting Germany NUTS configuration")
+        germany.write(
+            {
+                "state_level": 2,
+            }
+        )
