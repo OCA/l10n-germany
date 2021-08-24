d4433885b4a9c7275532377ff4386b745a35b2d2
dsolanki <dhara.solanki@initos.com>
2021-08-24 17:22:00 +0530

    [MIG] l10n_de_skr04_mis_reports: Migration to 14.0

--- l10n_de_skr04_mis_reports/__manifest__.py
+++ l10n_de_skr04_mis_reports/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "OpenBIG.org," "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-germany",
     "category": "Reporting",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "mis_builder",  # OCA/account-financial-reporting
