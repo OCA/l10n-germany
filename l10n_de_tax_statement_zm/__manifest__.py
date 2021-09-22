01bae542df58c9be71bf02b6f92d2df8cb0b97ec
hkapatel <helly.kapatel@initos.com>
2021-09-22 14:49:06 +0530

    [IMP] l10n_de_tax_statement: black, isort, prettier

--- l10n_de_tax_statement_zm/__manifest__.py
+++ l10n_de_tax_statement_zm/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'German VAT Statement Extension',
-    'version': '11.0.1.0.0',
-    'category': 'Localization',
-    'license': 'AGPL-3',
-    'author': 'OpenBIG.org, Onestein, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-germany',
-    'depends': [
-        'l10n_de_tax_statement',
+    "name": "German VAT Statement Extension",
+    "version": "11.0.1.0.0",
+    "category": "Localization",
+    "license": "AGPL-3",
+    "author": "OpenBIG.org, Onestein, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-germany",
+    "depends": [
+        "l10n_de_tax_statement",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/l10n_de_vat_statement_view.xml',
-        'views/report_tax_statement.xml',
-        'report/report_tax_statement.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/l10n_de_vat_statement_view.xml",
+        "views/report_tax_statement.xml",
+        "report/report_tax_statement.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- l10n_de_tax_statement_zm/tests/test_l10n_de_tax_statement_zm.py
+++ l10n_de_tax_statement_zm/tests/test_l10n_de_tax_statement_zm.py
@@ -3,12 +3,12 @@
 
 from odoo.exceptions import UserError, ValidationError
 
-from odoo.addons.l10n_de_tax_statement.tests.test_l10n_de_tax_statement\
-    import TestVatStatement
+from odoo.addons.l10n_de_tax_statement.tests.test_l10n_de_tax_statement import (
+    TestVatStatement,
+)
 
 
 class TestTaxStatementZM(TestVatStatement):
-
     def _prepare_zm_invoice(self):
         for invoice_line in self.invoice_1.invoice_line_ids:
             for tax_line in invoice_line.invoice_line_tax_ids:
@@ -18,29 +18,37 @@ class TestTaxStatementZM(TestVatStatement):
         self.statement_with_zm.statement_update()
 
     def test_01_compute_tag_41(self):
-        self.statement_with_zm = self.env['l10n.de.tax.statement'].create({
-            'name': 'Statement 1',
-        })
+        self.statement_with_zm = self.env["l10n.de.tax.statement"].create(
+            {
+                "name": "Statement 1",
+            }
+        )
 
         self.assertEqual(self.statement_with_zm.tag_41_base, self.tag_3)
         self.assertEqual(self.statement_with_zm.tag_21_base, self.tag_4)
 
     def test_02_no_tag_41(self):
-        self.config.write({
-            'tag_41_base': False,
-            'tag_21_base': False,
-        })
-        self.statement_not_valid = self.env['l10n.de.tax.statement'].create({
-            'name': 'Statement 1',
-        })
+        self.config.write(
+            {
+                "tag_41_base": False,
+                "tag_21_base": False,
+            }
+        )
+        self.statement_not_valid = self.env["l10n.de.tax.statement"].create(
+            {
+                "name": "Statement 1",
+            }
+        )
         self.statement_not_valid.statement_update()
         with self.assertRaises(UserError):
             self.statement_not_valid.post()
 
     def test_03_post_final(self):
-        self.statement_with_zm = self.env['l10n.de.tax.statement'].create({
-            'name': 'Statement 1',
-        })
+        self.statement_with_zm = self.env["l10n.de.tax.statement"].create(
+            {
+                "name": "Statement 1",
+            }
+        )
 
         # all previous statements must be already posted
         self.statement_with_zm.statement_update()
@@ -49,19 +57,19 @@ class TestTaxStatementZM(TestVatStatement):
 
         self.statement_1.statement_update()
         self.statement_1.post()
-        self.assertEqual(self.statement_1.state, 'posted')
+        self.assertEqual(self.statement_1.state, "posted")
 
         # first post
         self.statement_with_zm.post()
 
-        self.assertEqual(self.statement_with_zm.state, 'posted')
+        self.assertEqual(self.statement_with_zm.state, "posted")
         self.assertTrue(self.statement_with_zm.date_posted)
 
         self.statement_with_zm.zm_update()
 
         # then finalize
         self.statement_with_zm.finalize()
-        self.assertEqual(self.statement_with_zm.state, 'final')
+        self.assertEqual(self.statement_with_zm.state, "final")
         self.assertTrue(self.statement_with_zm.date_posted)
 
         with self.assertRaises(UserError):
@@ -69,11 +77,13 @@ class TestTaxStatementZM(TestVatStatement):
 
     def test_04_zm_invoice(self):
         self.statement_1.post()
-        self.statement_with_zm = self.env['l10n.de.tax.statement'].create({
-            'name': 'Statement 1',
-        })
+        self.statement_with_zm = self.env["l10n.de.tax.statement"].create(
+            {
+                "name": "Statement 1",
+            }
+        )
 
-        self.invoice_1.partner_id.country_id = self.env.ref('base.be')
+        self.invoice_1.partner_id.country_id = self.env.ref("base.be")
         self._prepare_zm_invoice()
 
         self.statement_with_zm.post()
@@ -90,11 +100,13 @@ class TestTaxStatementZM(TestVatStatement):
 
     def test_05_zm_invoice_service(self):
         self.statement_1.post()
-        self.statement_with_zm = self.env['l10n.de.tax.statement'].create({
-            'name': 'Statement 1',
-        })
+        self.statement_with_zm = self.env["l10n.de.tax.statement"].create(
+            {
+                "name": "Statement 1",
+            }
+        )
 
-        self.invoice_1.partner_id.country_id = self.env.ref('base.be')
+        self.invoice_1.partner_id.country_id = self.env.ref("base.be")
         for invoice_line in self.invoice_1.invoice_line_ids:
             for tax_line in invoice_line.invoice_line_tax_ids:
                 tax_line.tag_ids = self.tag_4
@@ -116,11 +128,13 @@ class TestTaxStatementZM(TestVatStatement):
 
     def test_06_zm_invoice_de(self):
         self.statement_1.post()
-        self.statement_with_zm = self.env['l10n.de.tax.statement'].create({
-            'name': 'Statement 1',
-        })
+        self.statement_with_zm = self.env["l10n.de.tax.statement"].create(
+            {
+                "name": "Statement 1",
+            }
+        )
 
-        self.invoice_1.partner_id.country_id = self.env.ref('base.de')
+        self.invoice_1.partner_id.country_id = self.env.ref("base.de")
         self._prepare_zm_invoice()
 
         with self.assertRaises(ValidationError):
@@ -128,11 +142,13 @@ class TestTaxStatementZM(TestVatStatement):
 
     def test_07_zm_invoice_outside_europe(self):
         self.statement_1.post()
-        self.statement_with_zm = self.env['l10n.de.tax.statement'].create({
-            'name': 'Statement 1',
-        })
+        self.statement_with_zm = self.env["l10n.de.tax.statement"].create(
+            {
+                "name": "Statement 1",
+            }
+        )
 
-        self.invoice_1.partner_id.country_id = self.env.ref('base.us')
+        self.invoice_1.partner_id.country_id = self.env.ref("base.us")
         self._prepare_zm_invoice()
 
         with self.assertRaises(ValidationError):
