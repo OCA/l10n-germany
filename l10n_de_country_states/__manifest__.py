f9f7b40378ed57ff6872e8c74696c24964bc7097
Achraf Mhadhbi <machraf@bloopark.de>
2022-04-29 10:41:51 +0200

    [MIG] l10n_de_country_states: Migration to 15.0

--- l10n_de_country_states/__manifest__.py
+++ l10n_de_country_states/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "German Country States",
     "category": "Localisation/Europe",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "initOS GmbH" "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-germany",
