02b40a1b8d53910138693443a85fc4e7c4e61c18
fkantelberg <florian.kantelberg@initos.com>
2023-03-03 09:28:03 +0100

    [ADD] datev_export_xml: Add module to export account.move as DATEV XML

---
+++ datev_export_xml/tests/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2022-2023 initOS GmbH
+# Copyright (C) 2020, Elego Software Solutions GmbH, Berlin
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import test_datev_export, test_generator
