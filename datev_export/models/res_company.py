b31a2afd8da2e92563b7592c9bb4d6e938e0e34b
fkantelberg <florian.kantelberg@initos.com>
2023-05-31 11:50:08 +0200

    [MIG] datev_export_xml: Migration to 15.0

--- datev_export/models/res_company.py
+++ datev_export/models/res_company.py
@@ -8,13 +8,13 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     datev_consultant_number = fields.Char(
-        string="Consultant Number",
+        "Consultant Number",
         size=8,
         help="Number from 1000 to 99999999",
     )
 
     datev_client_number = fields.Char(
-        string="Client Number",
+        "Client Number",
         size=5,
         help="Number from 0 to 99999",
     )
