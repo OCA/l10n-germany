0192b7cce1ce99fe33eb8b3a0d0af1284c93ac7d
Mark Schuit <mark@gig.solutions>
2024-01-19 16:51:59 +0800

    [MIG] 16.0 l10n_de_tax_statement - Migration to 16.0

--- l10n_de_tax_statement/__manifest__.py
+++ l10n_de_tax_statement/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "German VAT Statement",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Localization",
     "license": "AGPL-3",
     "author": "Onestein, Odoo Community Association (OCA)",
