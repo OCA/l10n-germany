fe663edad6337dab4cd495931fa6c20fc6f175c7
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2025-01-31 17:17:50 +0100

    [IMP] l10n_de_skr04_mis_reports: pre-commit stuff

--- l10n_de_skr04_mis_reports/__manifest__.py
+++ l10n_de_skr04_mis_reports/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "OpenBIG.org," "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-germany",
     "category": "Reporting",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "mis_builder",  # OCA/account-financial-reporting
