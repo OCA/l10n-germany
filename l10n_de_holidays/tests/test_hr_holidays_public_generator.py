7b3fd78a1ee378e620462980e7cfde84e5871989
Alexander Bigga <alexander.bigga@verdigado.com>
2025-09-27 22:50:46 +0200

    [IMP] l10n_de_holidays: add tests for german holiday creation

--- l10n_de_holidays/tests/test_hr_holidays_public_generator.py
+++ l10n_de_holidays/tests/test_hr_holidays_public_generator.py
@@ -2,6 +2,8 @@
 # Copyright 2018 initOS GmbH - Nikolina Todorova
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from datetime import date
+
 from odoo.exceptions import UserError
 
 from . import common
@@ -24,6 +26,38 @@ class TestHrHolidaysPublicGenerator(common.TestHolidaysGenerator):
             if not line_ids:
                 line_ids = None
             self.assertIsNotNone(line_ids)
+
+    def test_calculate_fixed_holidays(self):
+        self.hr_holidays_public_generator.action_run()
+
+        hr_holiday_public = self.HrHolidaysPublic.search(
+            [
+                ("year", "=", self.TestYear),
+                ("country_id", "=", self.CountryId),
+            ]
+        )
+        self.assertTrue(hr_holiday_public, "No public holiday created.")
+
+        expected_dates = {
+            date(self.TestYear, 1, 1),
+            date(self.TestYear, 5, 1),
+            date(self.TestYear, 10, 3),
+            date(self.TestYear, 12, 25),
+            date(self.TestYear, 12, 26),
+        }
+
+        fixed_dates = set(hr_holiday_public.line_ids.mapped("date"))
+
+        self.assertTrue(
+            expected_dates.issubset(fixed_dates),
+            f"Expected holidays: {expected_dates}, found: {fixed_dates}",
+        )
+
+        for line in hr_holiday_public.line_ids:
+            if line.date in expected_dates:
+                self.assertFalse(
+                    line.variable_date, f"{line.date} should not be variable date."
+                )
 
     def test_action_copy_de_holidays(self):
         self.hr_holidays_public_generator.action_generate_de_holidays()
@@ -109,3 +143,19 @@ class TestHrHolidaysPublicGenerator(common.TestHolidaysGenerator):
 
         with self.assertRaises(UserError):
             hr_holidays_public_generator_copy.action_run()
+
+    def test_calculate_repentance_day(self):
+        # Repentance Day 2023 should be on 22.11.2023.
+        result_2023 = self.hr_holidays_public_generator.calculate_repentance_day(2023)
+        self.assertTrue(result_2023, "No date calculated for 2023")
+        self.assertEqual(result_2023, "2023-11-22")
+
+        # Repentance Day 2023 should be on 20.11.2024.
+        result_2024 = self.hr_holidays_public_generator.calculate_repentance_day(2024)
+        self.assertTrue(result_2024, "No date calculated for 2024")
+        self.assertEqual(result_2024, "2024-11-20")
+
+        # Repentance Day 2023 should be on 19.11.2025.
+        result_2025 = self.hr_holidays_public_generator.calculate_repentance_day(2025)
+        self.assertTrue(result_2025, "No date calculated for 2025")
+        self.assertEqual(result_2025, "2025-11-19")
