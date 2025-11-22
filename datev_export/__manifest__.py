f3d7e6b3ef823f51bab31f946b703e7a115a8502
Christopher Rogos <crogos@gmail.com>
2025-11-22 20:37:09 +0000

    [MIG] datev_export: Migration to 18.0

--- datev_export/__manifest__.py
+++ datev_export/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Datev Export",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "Guenter Selbert, Thorsten Vocks, Maciej Wichowski, Daniela Scarpa, "
