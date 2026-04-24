dc52c44ccb4599f7da9dc73b01912a2e2eea6d58
Ole <Ole Jancke>
2026-04-24 15:46:26 +0200

    [IMP] get rid of code redundance

--- l10n_de_invoice_labor_cost/models/product_template.py
+++ l10n_de_invoice_labor_cost/models/product_template.py
@@ -12,10 +12,17 @@ class ProductTemplate(models.Model):
         help="Indicates whether this product is a labor product.",
     )
 
-    @api.onchange("is_labor_cost_product", "type")
+    def _check_is_labor_cost_product(self):
+        self.ensure_one()
+        if self.is_labor_cost_product and self.type in ("consu", "combo"):
+            raise ValidationError(
+                _("Labor cost products cannot be of type 'Goods' or 'Combo'.")
+            )
+
+    @api.constrains("is_labor_cost_product", "type")
     def _check_labor_cost_product_type(self):
-        for record in self:
-            if record.is_labor_cost_product and record.type in ("consu", "combo"):
-                raise ValidationError(
-                    _("Labor cost products cannot be of type 'Goods' or 'Combo'.")
-                )
+        self._check_is_labor_cost_product()
+
+    @api.onchange("is_labor_cost_product", "type")
+    def _onchange_check_labor_cost_product_type(self):
+        self._check_labor_cost_product_type()
